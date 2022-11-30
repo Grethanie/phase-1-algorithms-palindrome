b49a660f8fc80067b003972e0d0bc1eabb0aa79d
Daniel Young <Danyoung91@gmail.com>
2022-11-30 12:21:26 -0500

    commit tree c84764c3629762488dbd0f50d462eaf4e975eaf5

--- index.js
+++ index.js
@@ -1,13 +1,33 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+  let characters = word.split("");
+
+  for (let i = 0; i < characters.length / 2; i++) {
+    if (characters[i] !== characters[characters.length - i - 1]) {
+      return false;
+    }
+  }
+  return true;
 }
 
 /* 
-  Add your pseudocode here
+  initialize an array called charaters with has in it the split string
+
+
+  iterate through the first half of the array
+    if the element and it's compliment are identical, keep going
+    if not, then return false
+
+    return true
 */
 
 /*
-  Add written explanation of your solution here
+  first break the input string into characters
+
+  then put the characters into an array
+
+  compare the first and last members through to the middle
+
+  if at any point it comes back negative, then the string is not a palindrome and the funtion returns false.
 */
 
 // You can run `node index.js` to view these console logs
